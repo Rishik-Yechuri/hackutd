4076ff4e6d5259001f53040885e845d3fabf6ca3
srinashg <51180003+srinashg@users.noreply.github.com>
2022-11-12 14:00:27 -0600

    First change

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
+  //ILovepuss
+  
+  gpriygyw80ghwr8oreturn (
+    <div className="App69">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -15,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn JS
         </a>
       </header>
     </div>
